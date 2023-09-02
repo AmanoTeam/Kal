fb2fd0478582ae45f4d4d2d111950c8062b2fef8
Kartatz <105828205+Kartatz@users.noreply.github.com>
2023-09-02 03:32:24 -0300

    Add missing quote

--- arm-kobo-linux-gnueabihf.sh
+++ arm-kobo-linux-gnueabihf.sh
@@ -4,4 +4,4 @@ declare extra_configure_flags='--with-cpu=cortex-a8 --with-fpu=neon --with-float
 
 declare triplet='arm-kobo-linux-gnueabihf'
 
-declare sysroot='https://web.archive.org/web/0if_/https://github.com/koreader/koxtoolchain/releases/latest/download/kobo.zip
+declare sysroot='https://web.archive.org/web/0if_/https://github.com/koreader/koxtoolchain/releases/latest/download/kobo.zip'
