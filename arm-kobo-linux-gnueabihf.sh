57b56821b37625348416ca00e5658cdde1e8576b
Kartatz <105828205+Kartatz@users.noreply.github.com>
2023-09-02 02:17:13 -0300

    Fix toolchain name

--- arm-kobo-linux-gnueabihf.sh
+++ arm-kobo-linux-gnueabihf.sh
@@ -2,6 +2,6 @@
 
 declare extra_configure_flags='--with-cpu=cortex-a8 --with-fpu=neon --with-float=softfp --with-mode=thumb'
 
-declare triplet='arm-bookeen-linux-gnueabi'
+declare triplet='arm-kobo-linux-gnueabihf'
 
-declare sysroot='https://web.archive.org/web/0if_/https://github.com/koreader/koxtoolchain/releases/latest/download/bookeen.zip'
+declare sysroot='https://web.archive.org/web/0if_/https://github.com/koreader/koxtoolchain/releases/latest/download/kobo.zip
